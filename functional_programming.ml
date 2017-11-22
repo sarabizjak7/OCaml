88e8f32d4c17ff473cf8f56877ef0868998ae221
Sara Bizjak <sarabizjak97@gmail.com>
2017-11-22 13:11:47 +0100

    Add files

---
+++ functional_programming.ml
@@ -0,0 +1,239 @@
+(* ===== Vaja 2: Funkcijsko Programiranje  ===== *)
+
+(*Namig: Napiši si pomožno funkcijo za obračanje seznamov. *)
+
+let rec reverse l = 
+	match l with
+	| [] -> []
+	| hd :: tl -> reverse tl @  [hd]
+
+  
+(* Funkcija "repeat x n" vrne seznam n ponovitev x. 
+ Za neprimerne n funkcija vrne prazen seznam.
+ ----------
+ # repeat "A" 5;;
+ - : string list = ["A"; "A"; "A"; "A"; "A"]
+ # repeat "A" (-2);;
+ - : string list = []
+ ---------- *)
+
+let rec repeat x n = 
+	if n < 0 then [] else
+		match (x,n) with
+		| (_,0) -> [] (*base case*)
+		| (x,n) -> let recursive_result = repeat x (n-1)
+		in 
+		x :: recursive_result
+	
+	
+let rec repeat' x n =
+	if n <= 0 
+	then 
+		[] (*base case*)
+	else
+		let rec_res = repeat x (n-1) 
+		in
+		x :: rec_res
+		
+(* Funkcija "range n" sprejme številio n in vrne seznam vseh celih števil od 0
+ do vključno n. Za neprimerne n funkcija vrne prazen seznam.
+ Funkcija je repno rekurzivna.
+ ----------
+ # range 10;;
+ - : int list = [0; 1; 2; 3; 4; 5; 6; 7; 8; 9; 10]
+ ---------- *)
+
+let rec range n = (*ni učinkovito na večjih številih, ker se mora zapeljati čez cel seznam*)
+	if n < 0 then []
+	else
+		match n with
+		| 0 -> [0]
+		| _ -> let ran = range (n-1)
+		in 
+		ran @ [n]
+		
+
+let range' n = (*bolj učinkovito, ker se mu ni treba zapeljat čez cel seznam, ampak pripenja na zečetek*)
+	let rec range_down n acc =
+		if n < 0 then acc
+		else
+			range_down (n-1) (n :: acc)
+			in 
+			range_down n []
+			
+
+
+(* Funkcija "map f l" sprejme seznam l = [l0; l1; l2; ...] in funkcijo f
+ in vrne seznam [f(l0); f(l1); f(l2); ...].
+ ----------
+ # let plus_two = (+)2 in
+   map plus_two [0; 1; 2; 3; 4];;
+ - : int list = [2; 3; 4; 5; 6]
+ ---------- *)
+
+let rec map f l = 
+	match l with
+	| [] -> []
+	| hd :: tl -> f hd :: map f tl
+	
+		
+
+(* Funkcija "map_tlrec" je tail-recursive verzija funkcije map.
+ ----------
+ # let plus_two = (fun x -> x+2) in
+   map_tlrec plus_two [0; 1; 2; 3; 4];;
+ - : int list = [2; 3; 4; 5; 6]
+ ---------- *)
+
+let map_tlrec f l = 
+	let rec map_aux acc l =
+		match l with
+		| hd :: tl -> map_aux (f hd :: acc) tl
+		| [] -> reverse acc
+		in 
+		map_aux [] l
+
+(* Funkcija "mapi f l" sprejme seznam l = [l0; l1; l2; ...] in funkcijo f
+ ter vrne seznam [f 0 l0; f 1 l1; f 2 l2; ...].
+ ----------
+ # mapi (+) [0; 0; 0; 2; 2; 2];;
+ - : int list = [0; 1; 2; 5; 6; 7]
+ ---------- *)
+
+let rec mapi f l = 
+	let rec mapping l acc i =
+		match l with
+		| [] -> reverse acc
+		| hd :: tl -> mapping tl ((f i hd) :: acc) (i+1)
+		in
+		mapping l [] 0
+	
+
+
+(* Funkcija "zip l1 l2" sprejme seznama l1 = [l1_0; l1_1; l1_2; ...] in
+ l2 = [l2_0; l2_1; l2_2; ...] in vrne seznam [(l1_0,l2_0); (l1_1,l2_1); ...].
+ Če seznama nista enake dolžine vrne napako.
+ ----------
+ # zip [1; 1; 1; 1] [0; 1; 2; 3];;
+ - : (int * int) list = [(1, 0); (1, 1); (1, 2); (1, 3)]
+ # zip [1; 1; 1; 1] [1; 2; 3; 4; 5];;
+ Exception: Failure "Seznama razlicnih dolzin.".
+ ---------- *)
+
+let zip l1 l2 = ()
+
+(* Funkcija "zip_enum_tlrec l1 l2" sprejme seznama l1 = [l1_0; l1_1; l1_2; ...] 
+ in l2 = [l2_0; l2_1; l2_2; ...] in vrne [(0, l1_0, l2_0); (1, l1_1, l2_1); ...].
+ Funkcija je repno rekurzivna.
+ Če seznama nista enake dolžine vrne napako.
+ ----------
+ # zip_enum_tlrec ["a"; "b"; "c"; "d"] [7; 3; 4; 2];;
+ - : (int * string * int) list = [(0, "a", 7); (1, "b", 3); (2, "c", 4); (3, "d", 2)]
+ ---------- *)
+
+let zip_enum_tlrec l1 l2 = ()
+
+(* Funkcija "unzip l" sprejme seznam l = [(a0, b0); (a1, b2); ...]
+ in vrne dvojico seznamov ([a0; a1; ...], [b0; b1; ...]).
+ ----------
+ # unzip [(0,"a"); (1,"b"); (2,"c")];;
+ - : int list * string list = ([0; 1; 2], ["a"; "b"; "c"])
+ ---------- *)
+
+let unzip l = ()
+
+(* Funkcija "unzip_tlrec l" je tail-recursive verzija funkcije unzip.
+ ----------
+ # unzip_tlrec [(0,"a"); (1,"b"); (2,"c")];;
+ - : int list * string list = ([0; 1; 2], ["a"; "b"; "c"])
+ ---------- *)
+
+let unzip_tlrec l = ()
+
+(* Funkcija "fold_left_no_acc f l" sprejme seznam l = [l0; l1; l2; ...; ln] in funkcijo f,
+ vrne pa f(... (f (f (f l0 l1) l2) l3) ... ln).
+ V primeru seznama z manj kot dvema elementoma vrne napako.
+ ----------
+ # fold_left_no_acc (^) ["F"; "I"; "K"; "U"; "S"];;
+ - : string = "FIKUS"
+ ---------- *)
+
+let fold_left_no_acc f l = ()
+
+(* Funkcija "apply_sequence f x n" vrne seznam zaporednih uporab funkcije f na x,
+ [x; f x; f (f x); ...; f uporabljena n-krat na x].
+ Funkcija je repno rekurzivna.
+ ----------
+ # apply_sequence (fun x -> x*x) 2 5;;
+ - : int list = [2; 4; 16; 256; 65536; 4294967296]
+ # apply_sequence (fun x -> x*x) 2 (-5);;
+ - : int list = []
+ ---------- *)
+
+let apply_sequence f x n = ()
+
+(* Funkcija "filter f l" sprejme seznam l in vrne seznam elementov l,
+ za katere funkcija f vrne true.
+ ----------
+ # filter ((<)3) [0; 1; 2; 3; 4; 5];;
+ - : int list = [4; 5]
+ ---------- *)
+
+let filter f l = ()
+
+(* Funkcija "exists f l" sprejme seznam l in vrne true če obstaja 
+element
+ seznama l, za katerega fukcija f vrne true in false sicer.
+ Funkcija je repno rekurzivna.
+ ----------
+ # exists ((<)3) [0; 1; 2; 3; 4; 5];;
+ - : bool = true
+ # exists ((<)8) [0; 1; 2; 3; 4; 5];;
+ - : bool = false
+ ---------- *)
+
+let exists f l = ()
+
+(* Funkcija "first f none_value l" sprejme seznam l in vrne prvi element seznama,
+ za katerega funkcija f vrne true, če takšnega elementa ni, pa vrne none_value.
+ Funkcija je repno rekurzivna.
+ ----------
+ # first ((<)3) 0 [1; 1; 2; 3; 5; 8];;
+ - : int = 5
+ # first ((<)8) 0 [1; 1; 2; 3; 5; 8];;
+ - : int = 0
+ ---------- *)
+
+let first f none_value l = ()
+  
+(* Severnjaki napadajo Medbrezje. Kot vrhovni čarodej poznaš zaporedje urokov s katerimi
+ lahko Medbrezje zaščitiš pred napadom, zaporedje urokov pa je predstavljeno v seznamu oblike
+ [("ime1", vrednost1); ("ime2", vrednost2); ...].
+ Na razpolago imaš skupino čarodejov, ki so prav tako predstavljeni v seznamu oblike
+ [("ime1", spretnost1); ("ime2", spretnost2); ...].
+ Čarodej lahko izvede zaporedje urokov, če je njegova spretnost večja ali enaka skupni
+ vrednosti vseh urokov v zaporedju.
+ Funkcija "able_protectors spells wizards" vrne seznam imen vseh čarodejov, ki lahko
+ samostojno zaščitijo Medbrezje.
+ Funkcija "fails_on spells wizards" vrne seznam parov (čarodej, neuspešni urok), kjer
+ je neuspešni urok prvi urok v zaporedju, za katerega čarodej nima več dovolj spretnosti.
+ Če lahko čarodej zaporedje izvede v celoti, to predstavlja prazen niz.
+ Namig: Dober čarodej uporablja svoje znanje in izkušnje, ki jih pridobi tekom učenja.
+ ----------
+ # let spells = [("Protect",51); ("Renounce", 17); ("Blaze", 420); ("Banish",103)] in
+   let wizards = [("Merlin", 1832); ("Frodo", 53); ("Atijam", 1337);
+  ("Mr Duck", 7); ("Kylo Ren", 589); ("Snoop Dogg", 123)] in
+   able_protectors spells wizards;;
+ - : string list = ["Merlin"; "Atijam"]
+ ----------
+ # let spells = [("Protect",51); ("Renounce", 17); ("Blaze", 420); ("Banish",103)] in
+   let wizards = [("Merlin", 1832); ("Frodo", 53); ("Atijam", 1337);
+  ("Mr Duck", 7); ("Kylo Ren", 589); ("Snoop Dogg", 123)] in
+   fails_on spells wizards;;
+ - : (string * string) list = [("Merlin", ""); ("Frodo", "Renounce"); ("Atijam", "");
+  ("Mr Duck", "Protect"); ("Kylo Ren", "Banish"); ("Snoop Dogg", "Blaze")]
+ ----------*)
+
+let able_protectors spells wizards = ()
+
+let fails_on spells wizards = ()
